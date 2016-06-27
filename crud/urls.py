848bc1dc75a7b8b7c5db74c70c927879d2897b03
douradofefs <fernandaghjps@gmail.com>
2016-06-26 21:22:58 -0300

    Detail view

--- crud/urls.py
+++ crud/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
-from .views import CreateElement
+from .views import CreateElement, DetailElement
 
 urlpatterns = [
     url(r'^element/new/$', CreateElement.as_view(), name='element_new'),
+	url(r'^element/(?P<pk>[0-9]+)/$', DetailElement.as_view(), name='element_detail'),
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from .models import Element 
 
 class CreateElement(CreateView):
 	model = Element
 	fields = ('code', 'text')
+
+class DetailElement(DetailView):
+	model = Element 
+
