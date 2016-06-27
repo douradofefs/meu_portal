eb7f6e40448c911bf77e7d4215b5d6a00f70246b
douradofefs <fernandaghjps@gmail.com>
2016-06-26 22:17:36 -0300

    listagem

--- crud/urls.py
+++ crud/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
-from .views import CreateElement, DetailElement, UpdateElement, DeleteElement
+from .views import ListElement, CreateElement, DetailElement, UpdateElement, DeleteElement
 
 urlpatterns = [
+	url(r'^$', ListElement.as_view(), name='element_list'),
     url(r'^element/new/$', CreateElement.as_view(), name='element_new'),
 	url(r'^element/(?P<pk>[0-9]+)/$', DetailElement.as_view(), name='element_detail'),
 	url(r'^element/edit/(?P<pk>[0-9]+)/$', UpdateElement.as_view(), name='element_update'),
--- crud/views.py
+++ crud/views.py
@@ -1,9 +1,16 @@
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import  ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 from .models import Element 
+
+
+class ListElement(ListView):
+	model = Element
+	paginate_by = 100
 
 class CreateElement(CreateView):
 	model = Element
 	fields = ('code', 'text')
+	success_url = reverse_lazy('element_list')
 
 class DetailElement(DetailView):
 	model = Element 
@@ -11,7 +18,8 @@ class DetailElement(DetailView):
 class UpdateElement(UpdateView):
 	model = Element
 	fields = ('code', 'text')
+	success_url = reverse_lazy('element_list')
 
 class DeleteElement(DeleteView):
 	model = Element
-
+	success_url = reverse_lazy('element_list')
