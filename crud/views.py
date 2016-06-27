50927a27851b11ab25cbbc537686d9469a63620b
douradofefs <fernandaghjps@gmail.com>
2016-06-26 23:21:30 -0300

    random

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,5 @@
+import requests
+import random
 from django.views.generic import  ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from .models import Element 
@@ -21,10 +23,19 @@ class ListElement(ListView):
 		context['form'] = ElementForm()
 		return context
 
+def get_text_from_api():
+	response = requests.get('http://api.randomuser.me/')
+	js = response.json()
+	return js.get('results')[0].get('location').get('street')
+
+def randomize():
+	return random.randrange(100000)
+
 class CreateElement(CreateView):
 	model = Element
 	fields = ('code', 'text')
 	success_url = reverse_lazy('element_list')
+	initial = {'code': randomize, 'text': get_text_from_api}
 
 class DetailElement(DetailView):
 	model = Element 
